c5870ed52099545b806d7f39f882b5cd91dba5ee
janip1 <jani.poklar@gmail.com>
2019-08-01 18:37:13 +0200

    corrections, burger menu, sass corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
+//Sticky header
 jQuery(window).scroll(function() {
     var sticky = jQuery('header')
     scroll = jQuery(window).scrollTop()
 
     if (scroll >= 10) sticky.addClass('fixed')
     else sticky.removeClass('fixed')
+})
+
+//Mobile burger menu
+jQuery(function($) {
+    $('.menu-btn').click(function() {
+        $('.responsive-menu').toggleClass('expand')
+    })
 })
